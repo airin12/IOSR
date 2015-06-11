45e6d54185b3553d9ee549287f59dedf6d336d14
slakomy <s.lakomy@gmail.com>
2015-06-11 13:53:38 -0700

    Fixed ConfigurationTest

--- src/test/java/pl/edu/agh/iosr/config/ConfigurationTest.java
+++ src/test/java/pl/edu/agh/iosr/config/ConfigurationTest.java
@@ -12,7 +12,8 @@ public class ConfigurationTest {
 
 	private final String noModeMessage = "You must specify mode parameter";
 	private final String noMetricMessage = "You must specify metric parameter";
-	private final String noAddressMessage = "You must specify address parameter";
+	private final String noServiceAddressMessage = "You must specify service_address parameter";
+	private final String noTsdbAddressMessage = "You must specify tsdb_address parameter";
 	private final String noTagsMessage = "You must specify tags parameter";
 	private final String noRequestsMessage = "You must specify req_nr parameter";
 	private final String noFileMessage = "You must specify file parameter";
@@ -35,18 +36,18 @@ public class ConfigurationTest {
 		assertEquals(false, config.isValid());
 		assertEquals(noMetricMessage, config.getErrorMsg());
 	}
-	
+
 	@Test
-	public void testNoAddressSet(){
+	public void testNoTsdbAddressParameterSet(){
 		String [] args = new String[]{"mode=LOAD","metric=agh"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
-		assertEquals(noAddressMessage, config.getErrorMsg());
+		assertEquals(noTsdbAddressMessage, config.getErrorMsg());
 	}
 	
 	@Test
 	public void testNoTagsSetInGenerateMode(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002"};
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
 		assertEquals(noTagsMessage, config.getErrorMsg());
@@ -54,7 +55,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testNoRequestsSetInGenerateMode(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 									  "tags=cpu:01"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
@@ -63,7 +64,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testNoFileSetInLoadMode(){
-		String [] args = new String[]{"mode=LOAD","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=LOAD","metric=agh","tsdb_address=localhost:2002",
 									  "tags=cpu:01"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
@@ -72,7 +73,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testNoSeparatorSetInLoadMode(){
-		String [] args = new String[]{"mode=LOAD","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=LOAD","metric=agh","tsdb_address=localhost:2002",
 									  "tags=cpu:01","file=data.txt"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
@@ -81,17 +82,26 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testNoFormatSetInLoadMode(){
-		String [] args = new String[]{"mode=LOAD","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=LOAD","metric=agh","tsdb_address=localhost:2002",
 									  "tags=cpu:01","file=data.txt","separator=:"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
 		assertEquals(noFormatMessage, config.getErrorMsg());
 	}
-	
+
+	@Test
+	public void testNoServiceAddressMessage(){
+		String [] args = new String[]{"mode=TEST1","metric=agh","tsdb_address=localhost:2002",
+				"tags=cpu:01","req_nr=100","file=data.txt"};
+		Configuration config = new Configuration(args);
+		assertEquals(false, config.isValid());
+		assertEquals(noServiceAddressMessage, config.getErrorMsg());
+	}
+
 	@Test
 	public void testNoAggregatorSetInLoadMode(){
-		String [] args = new String[]{"mode=TEST1","metric=agh","address=localhost:2002",
-									  "tags=cpu:01","req_nr=100","file=data.txt"};
+		String [] args = new String[]{"mode=TEST1","metric=agh","tsdb_address=localhost:2002",
+									  "tags=cpu:01","req_nr=100","file=data.txt", "service_address=127.0.0.1"};
 		Configuration config = new Configuration(args);
 		assertEquals(false, config.isValid());
 		assertEquals(noAggregatorMessage, config.getErrorMsg());
@@ -99,7 +109,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetNrOfRequests(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -108,7 +118,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetDelay(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","delay=100"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -117,7 +127,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetMax(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","max=100"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -126,7 +136,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetMin(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","min=100"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -135,7 +145,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetStart(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","start=100"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -144,7 +154,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetEnd(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","end=100"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -153,7 +163,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetAgr(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","aggregator=sum"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -162,7 +172,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetStep(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","step=2"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -171,7 +181,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetFile(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","step=2","file=data.txt"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -180,7 +190,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetDup(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","duplicate=2"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -189,7 +199,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetSeparator(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","separator=:"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -198,7 +208,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSetFormat(){
-		String [] args = new String[]{"mode=GENERATE","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=GENERATE","metric=agh","tsdb_address=localhost:2002",
 				  "tags=cpu:01","req_nr=100","format=timestamp:value:","separator=:"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
@@ -207,7 +217,7 @@ public class ConfigurationTest {
 	
 	@Test
 	public void testSettingFormatData(){
-		String [] args = new String[]{"mode=LOAD","metric=agh","address=localhost:2002",
+		String [] args = new String[]{"mode=LOAD","metric=agh","tsdb_address=localhost:2002",
 				  "file=data.txt","separator=:","format=timestamp:value:cpu"};
 		Configuration config = new Configuration(args);
 		assertTrue(config.isValid());
